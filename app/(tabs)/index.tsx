7a4edf76c32cc8b0de03093b8f38a3f363d93335
Szymon Lipka <szymonlipkaa@gmail.com>
2025-07-17 22:13:04 +0200

    test change

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -23,7 +23,7 @@ export default function HomeScreen() {
       <ThemedView style={styles.stepContainer}>
         <ThemedText type="subtitle">Step 1: Try it</ThemedText>
         <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
+          Test
           Press{' '}
           <ThemedText type="defaultSemiBold">
             {Platform.select({
